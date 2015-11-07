9201829e773b80df4d502c2120eb87ffe54a95e8
SMA8856 <SMA8856@TME-A-B-1400625.COMMON.EU.CORP.TOYOTA.com>
2015-11-07 19:30:03 +0100

    Second change

--- GitTutoarial/src/com/example/gittutoarial/FragmentActivity.java
+++ GitTutoarial/src/com/example/gittutoarial/FragmentActivity.java
@@ -2,10 +2,13 @@ package com.example.gittutoarial;
 
 import android.R.string;
 
+
 public class FragmentActivity {
 	
 	private int varB =0;
 	private int varC =0;
 
 	private String gitCommit = "Committing the changes directly in gitHub";
+	private String gitCommitSecond = "Committing the changes directly in gitHub Second change";
+	
 }
