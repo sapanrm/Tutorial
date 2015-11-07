c71109d04f2488cfe1205fd54e7638c20b6bc17b
SMA8856 <SMA8856@TME-A-B-1400625.COMMON.EU.CORP.TOYOTA.com>
2015-11-07 19:28:54 +0100

    Change to git

--- GitTutoarial/src/com/example/gittutoarial/FragmentActivity.java
+++ GitTutoarial/src/com/example/gittutoarial/FragmentActivity.java
@@ -1,8 +1,11 @@
 package com.example.gittutoarial;
 
+import android.R.string;
+
 public class FragmentActivity {
 	
 	private int varB =0;
 	private int varC =0;
 
+	private String gitCommit = "Committing the changes directly in gitHub";
 }
